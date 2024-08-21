9e165cfaed2ae94a1b068778d217be9e34f20b20
lgeti <ignat.pocevski@gmail.com>
2024-08-21 11:10:58 +0200

    Update App.vue and TransactionList.vue components
    The changes in this commit refactor the `App.vue` and `TransactionList.vue` components.
    
    In `App.vue`, the `TransactionList` component now receives the `transactions` prop, which is an array of transaction objects. This allows the component to dynamically render the transaction list based on the data provided.
    
    In `TransactionList.vue`, the static transaction list items have been replaced with a dynamic rendering using `v-for` directive. Each transaction is now rendered based on the `transactions` prop, with the transaction text, amount, and a delete button.

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,25 @@ import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+	{ id: 1, text: 'Flower', amount: -20 },
+	{ id: 2, text: 'Salary', amount: 300 },
+	{ id: 3, text: 'Book', amount: -10 },
+	{ id: 4, text: 'Camera', amount: 150 },
+	{ id: 5, text: 'Phone', amount: -50 }
+]);
+
 </script>
 
 <template>
-    <Header />
-    <div class="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-    </div>
+	<Header />
+	<div class="container">
+		<Balance />
+		<IncomeExpenses />
+		<TransactionList :transactions="transactions" />
+		<AddTransaction />
+	</div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,13 @@
+<script setup>
+
+const props = defineProps(['transactions']);
+
+</script>
+
+
 <template>
     <h3>History</h3>
       <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-          Paycheck <span>$400</span><button class="delete-btn">x</button>
-        </li>
+        <li v-for="transaction in transactions" v-bind:key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">{{ transaction.text }}<span>${{ transaction.amount }}</span><button class="delete-btn">x</button></li>
       </ul>
 </template>
