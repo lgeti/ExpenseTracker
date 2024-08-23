d040fe8b7e9e1821b39b84c75a4e18e00096e955
lgeti <ignat.pocevski@gmail.com>
2024-08-23 23:29:42 +0200

    saves to localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<template>
+	<Header />
+	<div class="container">
+		<Balance :total="+total"/>
+		<IncomeExpenses :income="+income" :expenses="+expenses"/>
+		<TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted"/>
+		<AddTransaction @transactionSubmitted="handleTransactionSubmitted"/>
+	</div>
+</template>
+
 <script setup>
 import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
@@ -5,16 +15,17 @@ import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-import { computed, ref } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 
-const transactions = ref([
-	{ id: 1, text: 'Flower', amount: -20 },
-	{ id: 2, text: 'Salary', amount: 300 },
-	{ id: 3, text: 'Book', amount: -10 },
-	{ id: 4, text: 'Camera', amount: 150 },
-	{ id: 5, text: 'Phone', amount: -50.32 }
-]);
+const transactions = ref([]);
 
+onMounted(() => {
+	const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+	if (savedTransactions) {
+		transactions.value = savedTransactions;
+	}
+});
 // Get total
 const total = computed(() => {
 	return transactions.value.reduce((acc, item) => (acc += item.amount), 0);
@@ -44,24 +55,25 @@ const handleTransactionSubmitted = (transactionData) => {
 		amount: transactionData.amount
 	});
 
+	saveTransactionsToLocalStorage();
+
 	toast.success('Transaction added');
 }
 
 // Delete transaction
 const handleTransactionDeleted = (id) => {
 	transactions.value = transactions.value.filter(transaction => transaction.id !== id);
+
+	saveTransactionsToLocalStorage();
+	
 	toast.success('Transaction deleted');
 }
 
+// Save to localStorage
+const saveTransactionsToLocalStorage = () => {
+	localStorage.setItem('transactions', JSON.stringify(transactions
+		.value));
+};
 
 </script>
 
-<template>
-	<Header />
-	<div class="container">
-		<Balance :total="+total"/>
-		<IncomeExpenses :income="+income" :expenses="+expenses"/>
-		<TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted"/>
-		<AddTransaction @transactionSubmitted="handleTransactionSubmitted"/>
-	</div>
-</template>
